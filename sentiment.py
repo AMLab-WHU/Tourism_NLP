b96293ae68d4c651014a925ac3ca3926a22230e5
Gary <56428019+Gary2018X@users.noreply.github.com>
2020-08-13 19:50:18 +0800

    格式化一下代码

--- sentiment.py
+++ sentiment.py
@@ -6,12 +6,14 @@ import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-## 使用snownlp获取文本情感值
+
+# 使用snownlp获取文本情感值
 def get_sentiment_cn(text):
     s = SnowNLP(text)
     return s.sentiments
 
-## 判断情感属性：积极 or 消极 or 中性
+
+# 判断情感属性：积极 or 消极 or 中性
 def get_sentimentbyself(score):
     if score > 0.5:
         s_byself = "positive"
@@ -21,26 +23,29 @@ def get_sentimentbyself(score):
         s_byself = "negative"
     return s_byself
 
-## 插入情感值和情感倾向
+
+# 插入情感值和情感倾向
 def insert_score():
     Data_admin = Database()
     sql_1 = 'select autoId, content from sentiment'
     data_1 = Data_admin.database(sql_1)
     sql_2 = 'select autoId, content from ctrip_comments'
     data_2 = Data_admin.database(sql_2)
-    data_list=[]
+    data_list = []
     for text in data_2:
         score = get_sentiment_cn(text[1])
         s_byself = get_sentimentbyself(score)
         if text in data_1:
-            sql_3 = 'update sentiment set Score="{}", Sentimentbyself="{}" where autoId={}'.format(score, s_byself, text[0])
+            sql_3 = 'update sentiment set Score="{}", Sentimentbyself="{}" where autoId={}'.format(score, s_byself,
+                                                                                                   text[0])
             Data_admin.database(sql_3)
         else:
             data_list.append((text[0], text[1], score, s_byself))
     sql = 'insert into sentiment(autoId,content,Score,Sentimentbyself) values (%s, %s, %s,%s)'
-    Data_admin.database(sql,data_list=data_list)
+    Data_admin.database(sql, data_list=data_list)
 
-## 时间数据处理：获取两时间点内的日期列表
+
+# 时间数据处理：获取两时间点内的日期列表
 def get_date_list():
     date_list = []
     start = input("请输入您想要查看的时间段的开始日期（如 2015-12-31）：").replace("/n", "")
@@ -52,19 +57,21 @@ def get_date_list():
         date_start += datetime.timedelta(days=1)
     return date_list
 
-## 对字典进行处理
+
+# 对字典进行处理
 def get_data_list(dict_data):
     dict_data = sorted(dict_data.items(), key=lambda x: x[0])  # 升序排序时间
     final_date_list = []  # 存储排序过的时间
-    data_list = [] # 存储对应的情感倾向人数列表
+    data_list = []  # 存储对应的情感倾向人数列表
     for m in dict_data:
         final_date_list.append(m[0])
         data_list.append(m[1])
-    return final_date_list,data_list
+    return final_date_list, data_list
 
-## 绘制情感倾向人数图
+
+# 绘制情感倾向人数图
 def draw_trend(date_list):
-    dict_data_p = {} # 存储时间与积极倾向评论数的对应关系
+    dict_data_p = {}  # 存储时间与积极倾向评论数的对应关系
     dict_data_n = {}  # 存储时间与消极倾向评论数的对应关系
     dict_data_z = {}  # 存储时间与中性倾向评论数的对应关系
     Data_admin = Database()
@@ -86,17 +93,18 @@ def draw_trend(date_list):
     final_date_p, data_list_p = get_data_list(dict_data_p)
     final_date_n, data_list_n = get_data_list(dict_data_n)
     final_date_z, data_list_z = get_data_list(dict_data_z)
-    if final_date_p == final_date_n == final_date_z :
+    if final_date_p == final_date_n == final_date_z:
         plt.figure(figsize=(10, 6))
-        plt.plot(final_date_p, data_list_p, marker='o',label='Positive numbers')
-        plt.plot(final_date_n, data_list_n, marker='o',label='Negative numbers')
-        plt.plot(final_date_z, data_list_z, marker='o',label='Neutual numbers')
+        plt.plot(final_date_p, data_list_p, marker='o', label='Positive numbers')
+        plt.plot(final_date_n, data_list_n, marker='o', label='Negative numbers')
+        plt.plot(final_date_z, data_list_z, marker='o', label='Neutual numbers')
         plt.title("Daily Trends")
-        plt.xticks(rotation=90, size = 8) #横坐标旋转90度
+        plt.xticks(rotation=90, size=8)  # 横坐标旋转90度
         plt.legend(loc='best')  # 显示上面的label
         plt.show()
 
-## 绘制情感值变化趋势图
+
+# 绘制情感值变化趋势图
 def draw_sentiment(date_list):
     dict_data = {}  # 存储时间与情感值的对应关系
     Data_admin = Database()
@@ -109,26 +117,26 @@ def draw_sentiment(date_list):
                 sentiment_c += text[1]
                 num += 1
         if num == 0:
-            dict_data[term] = 0   ## 没有数据的日期设置为0
+            dict_data[term] = 0  # 没有数据的日期设置为0
         else:
-            sentiment_a = sentiment_c / num  ## 求每日情感值的平均值
+            sentiment_a = sentiment_c / num  # 求每日情感值的平均值
             dict_data[term] = sentiment_a
     final_date, data_list = get_data_list(dict_data)
     plt.figure(figsize=(10, 6))
-    plt.plot(final_date, data_list, marker='o',label='Sentiments')
+    plt.plot(final_date, data_list, marker='o', label='Sentiments')
     plt.title("Daily Sentiments")
     plt.xticks(rotation=90, size=8)  # 横坐标旋转90度
     plt.legend(loc='best')  # 显示上面的label
     plt.show()
 
 
-## 执行程序
+# 执行程序
 if __name__ == '__main__':
     '''
     需要先在sentiment表上新建字段Score和Sentimentbyself
     Score为float类型，Sentimentbyself为varchar类型
     '''
-    insert_score()  ## 插入情感值和情感倾向
-    date_list = get_date_list() ## 时间数据处理：获取两时间点内的日期列表
-    draw_trend(date_list) ## 绘制情感倾向人数图
-    draw_sentiment(date_list) ## 绘制情感值变化趋势图
+    insert_score()  # 插入情感值和情感倾向
+    date_list = get_date_list()  # 时间数据处理：获取两时间点内的日期列表
+    draw_trend(date_list)  # 绘制情感倾向人数图
+    draw_sentiment(date_list)  # 绘制情感值变化趋势图

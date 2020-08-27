3829a02ae1e8c8444c5ac284ed678571a2607c5a
HQ0413 <63958268+HQ0413@users.noreply.github.com>
2020-08-27 23:05:40 +0800

    至今没有找到明确的分类标准

--- cluster.py
+++ cluster.py
@@ -3,6 +3,7 @@ import jieba
 import numpy as np
 import pandas as pd
 from jieba import analyse
+import jieba.posseg as pseg
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.feature_extraction.text import TfidfVectorizer  # 基于TF-IDF的词频转向量库
@@ -11,16 +12,20 @@ from data_admin import Database
 
 # 中文分词
 def seg_sentence(text):
-    sentence_seged = jieba.cut(text)
+    # sentence_seged = jieba.cut(text)
+    word_list = []  # 建立空列表用于存储分词结果
+    seg_list = pseg.cut(text)  # 精确模式分词[默认模式]
+    for word in seg_list:
+        if word.flag in ['n', 'ns', 'nt', 'nz', 'nrt', 'nrfg', 'nr', 'ng']:  # 选择属性
+            word_list.append(word.word)  # 分词追加到列表
     stopwords = [line.strip() for line in open('stopwords.txt', 'r', encoding='utf-8').readlines()]# 这里加载停用词的路径
     outstr = ''
-    for word in sentence_seged:
+    for word in word_list:
         if word not in stopwords:
             if word != '\t':
                 outstr += word
                 outstr += " "
     return outstr
-
 
 # 数据清理
 def data_clean(text):
@@ -38,32 +43,59 @@ def getKeywords_tfidf(text,num):
     kwds = " ".join(key_words_TFIDF)
     return kwds
 
-
-# K均值聚类
-def cluster(num, kwds_list):
+# 词向量模型
+def vectorizer(kwds_list):
     vectorizer = TfidfVectorizer()  # 创建词向量模型
     X = vectorizer.fit_transform(kwds_list)  # 将评论关键字列表转换为词向量空间模型
     word_vectors = vectorizer.get_feature_names()  # 词向量
     # print(word_vectors)
     word_values = X.toarray()  # 向量值
+    return word_values
+
+# K均值聚类
+def cluster(num, kwds_list):
+    word_values = vectorizer(kwds_list)
+    down_num = 10 # 降维维数
+    # 数据框标签
+    column = []
+    i = 1
+    while i <= down_num:
+        column.append('pca_'+str(i))
+        i += 1
+    # PCA降维
+    pca = PCA(n_components=down_num)  # 输出两维
+    newData = pca.fit_transform(word_values)  # 载入N维
     # K均值聚类
     model_kmeans = KMeans(n_clusters=num)  # 创建聚类模型对象,num指定类簇数量
-    model_kmeans.fit(X)  # 训练模型
+    model_kmeans.fit(newData)  # 训练模型
     # 聚类结果汇总
     cluster_labels = model_kmeans.labels_  # 聚类标签结果
-    # comment_matrix = np.hstack((word_values, cluster_labels.reshape(word_values.shape[0], 1)))  # 将向量值和标签值合并为新的矩阵
-    # word_vectors.append('cluster_labels')  # 将新的聚类标签列表追加到词向量后面
-    # comment_pd = pd.DataFrame(comment_matrix, columns=word_vectors)  # 创建包含词向量和聚类标签的数据框
-    # comment_pd.to_csv('comment.csv')
-    # PCA降维
-    pca = PCA(n_components=2)  # 输出两维
-    newData = pca.fit_transform(word_values)  # 载入N维
-    comment_matrix = np.hstack((newData, cluster_labels.reshape(newData.shape[0], 1)))  # 将向量值和标签值合并为新的矩阵
-    comment_pd = pd.DataFrame(comment_matrix, columns=['pca_x','pca_y','cluster_labels'])  # 创建包含词向量和聚类标签的数据框
+    column.append('cluster_labels')
+    column.append('keywords')
+    kwds_list = np.array(kwds_list)
+    comment_matrix = np.hstack((newData, cluster_labels.reshape(newData.shape[0], 1), kwds_list.reshape(newData.shape[0], 1)))  # 将向量值和标签值合并为新的矩阵
+    comment_pd = pd.DataFrame(comment_matrix, columns=column)  # 创建包含词向量和聚类标签的数据框
+    #word_vectors.append('cluster_labels')  # 将新的聚类标签列表追加到词向量后面
+    #comment_pd = pd.DataFrame(comment_matrix, columns=word_vectors)  # 创建包含词向量和聚类标签的数据框
+    comment_pd.to_csv('comment.csv')
+
+    # comment_matrix = np.hstack((newData, cluster_labels.reshape(newData.shape[0], 1)))  # 将向量值和标签值合并为新的矩阵
+    # comment_pd = pd.DataFrame(comment_matrix, columns=['pca_x','pca_y','cluster_labels'])  # 创建包含词向量和聚类标签的数据框
     # comment_pd.to_csv('pca.csv')
-    cluster_label = cluster_labels.reshape(newData.shape[0], 1)
+    # cluster_label = cluster_labels.reshape(newData.shape[0], 1)
     # print(cluster_label)
-    return cluster_label, newData
+    # return cluster_label, newData
+    return comment_pd
+
+# 输出每一类中句子的关键词
+def print_cluster(num, comment_pd):
+    i = 0
+    while i < num:
+        data_p = comment_pd[(comment_pd.cluster_labels == str(i))]['keywords']
+        data_p = np.array(data_p)
+        print('类别',i,'：')
+        print(data_p)
+        i+=1
 
 def draw_cluster(label, reduced_x):
     red_x, red_y = [], []
@@ -105,8 +137,10 @@ def main():
         item = data_clean(text[1]) # 数据清理
         # content_list.append(seg_sentence(item))
         kwds_list.append(getKeywords_tfidf(item,5)) #每句评论提取前5个关键词
-    label, reduced_x = cluster(5,kwds_list)  #设置类簇数量为5
-    draw_cluster(label, reduced_x)
+    # label, reduced_x = cluster(5, kwds_list)  #设置类簇数量为5
+    comment_pd = cluster(5, kwds_list)  #设置类簇数量为5
+    print_cluster(5, comment_pd) #设置类簇数量为5
+    # draw_cluster(label, reduced_x)
 
 if __name__ == '__main__':
     main()
